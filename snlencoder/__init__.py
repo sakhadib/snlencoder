b28237bf5e2d49593bd3104bff1ff6b266f1eed4
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-08-05 20:59:54 +0600

    Initialize SNL Encoder package with versioning and move encoding/decoding imports

---
+++ snlencoder/__init__.py
@@ -0,0 +1,9 @@
+"""
+SNL Encoder - A package for encoding and decoding game moves.
+"""
+
+from .encode import encode_move, decode_move
+
+__version__ = "1.0.0"
+__author__ = "sakhadib"
+__all__ = ["encode_move", "decode_move"]
